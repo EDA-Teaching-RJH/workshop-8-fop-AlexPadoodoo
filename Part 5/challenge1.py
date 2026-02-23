4f051c3fdb29c2e550ac6afd1302bb2755b670a7
AlexPadoodoo <app10@kent.ac.uk>
2026-02-23 11:07:34 +0000

    Finished the challenge

--- Part 5/challenge1.py
+++ Part 5/challenge1.py
@@ -3,5 +3,8 @@
 import re
 
 phone_number = input("Enter a Phone Number: ")
-
-if re.search(r"^07, \d, {m,11}$", phone_number)
+#phone number should start with 07 and have 11 numbers
+if re.search(r"^07\d{9}$", phone_number):
+    print("Valid phone number!")
+else:
+    print("Invalid phone number")
