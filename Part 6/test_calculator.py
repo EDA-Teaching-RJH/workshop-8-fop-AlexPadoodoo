5083079a2eb27a4182b808f8719071d43e873041
AlexPadoodoo <app10@kent.ac.uk>
2026-02-23 11:25:14 +0000

    step 3

--- Part 6/test_calculator.py
+++ Part 6/test_calculator.py
@@ -4,8 +4,10 @@ def main():
     test_square()
 
 def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
+    assert square(2) == 4:
+        print("2 squared was not 4")
+    assert square(3) == 9:
+        print("3 squared was not 9")
 
 
 if __name__ == "__main__":
