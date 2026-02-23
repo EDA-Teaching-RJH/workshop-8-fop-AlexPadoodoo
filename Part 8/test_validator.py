bb271818973526b285bc2a419321d752e06131ec
AlexPadoodoo <app10@kent.ac.uk>
2026-02-23 11:36:43 +0000

    finished the test validator

--- Part 8/test_validator.py
+++ Part 8/test_validator.py
@@ -10,3 +10,8 @@ def test_invalid_emails():
     assert is_valid_email("hello@wrold") == False
     assert is_valid_email("fake@kent.ac.uk.com") == False
     assert is_valid_email("no_at_symbol.ac.uk") == False
+
+    try:
+        assert is_valid_email("sneaky@@kent.ac.uk") == False
+    except AssertionError:
+        print("Warning: A sneaky email bypassed the REGEX!")
