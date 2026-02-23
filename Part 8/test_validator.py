19b7db7adfcf62e3f6a6225891b58f3ae7921eb8
AlexPadoodoo <app10@kent.ac.uk>
2026-02-23 11:35:05 +0000

    did part 4

--- Part 8/test_validator.py
+++ Part 8/test_validator.py
@@ -1,4 +1,12 @@
 from validator import is_valid_email
 
+#Step 3: Writing Assertions
 def test_valid_emails():
-    
+    assert is_valid_email("student1@kent.ac.uk") == True
+    assert is_valid_email("doctor@nhs.net") == True
+
+#Step 4: Segmenting Tests
+def test_invalid_emails():
+    assert is_valid_email("hello@wrold") == False
+    assert is_valid_email("fake@kent.ac.uk.com") == False
+    assert is_valid_email("no_at_symbol.ac.uk") == False
